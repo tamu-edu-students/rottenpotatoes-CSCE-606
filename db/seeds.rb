aa687840cb0000fb45c6e478c1c396c5cedf4d97
mohitsarin-tamu <mohitsarin26@tamu.edu>
2023-09-25 01:19:42 -0500

    Added new movies to the list

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,18 @@ more_movies = [
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
     :release_date => '13-Aug-2021'}
+  {:title => 'Harry Potter and the Sorcerer's Stone', :rating => 'PG-13',
+    :release_date => '13-Nov-2001'}
+  {:title => 'Harry Potter and the Chamber of Secrets ', :rating => 'PG-13',
+    :release_date => '15-Nov-2002'}
+  {:title => 'Harry Potter and the Prisoner of Azkaban', :rating => 'PG-13',
+    :release_date => '4-June-2004'}
+  {:title => 'Harry Potter and the Goblet of Fire', :rating => 'PG-13',
+    :release_date => '18-Nov-2005'}
+  {:title => 'Harry Potter and the Half-Blood Prince ', :rating => 'PG-13',
+    :release_date => '15-July-2009'}
+  {:title => 'M.S. Dhoni: The Untold Story', :rating => 'PG-13',
+    :release_date => '30-Sep-2016'}
 ]
 
 more_movies.each do |movie|
